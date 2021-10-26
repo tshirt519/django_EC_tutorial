69d19abd452011c2eafddda8a01ce1da15ebc574
tshirt519 <tshirt519@gmail.com>
2021-10-26 15:33:59 +0900

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+from django.views import View
+
+class ProfileView(View):
+  def get(self, request, *args, **kwargs):
+    return render(request, 'profile.html')
