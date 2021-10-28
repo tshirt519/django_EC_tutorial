7de46a85d22762d71f5a3d52ac91af8371f094b6
tshirt519 <tshirt519@gmail.com>
2021-10-28 15:03:18 +0900

    🎨 userモデルに住所と電話番号のデータを持たせるよう実装

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,6 +5,8 @@ class ProfileForm(forms.Form):
   first_name = forms.CharField(max_length=30, label='姓')
   last_name = forms.CharField(max_length=30, label='名')
   department = forms.CharField(max_length=30, label='所属', required=False)
+  address = forms.CharField(max_length=30, label='住所', required=False)
+  tel = forms.CharField(max_length=30, label='電話番号', required=False)
 
 class SignupUserForm(SignupForm):
   # first_name = forms.CharField(max_length=30, label='姓')
--- accounts/views.py
+++ accounts/views.py
@@ -21,6 +21,8 @@ class ProfileEditView(LoginRequiredMixin, View):
         'first_name': user_data.first_name,
         'last_name': user_data.last_name,
         'department': user_data.department,
+        'address': user_data.address,
+        'tel': user_data.tel,
       }
     )
 
@@ -35,6 +37,8 @@ class ProfileEditView(LoginRequiredMixin, View):
       user_data.first_name = form.cleaned_data['first_name']
       user_data.last_name = form.cleaned_data['last_name']
       user_data.department = form.cleaned_data['department']
+      user_data.address = form.cleaned_data['address']
+      user_data.tel = form.cleaned_data['tel']
       user_data.save()
       return redirect('profile')
 
