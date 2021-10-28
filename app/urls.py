f9a91845f931f98e30e65221b4abf5b16465378e
tshirt519 <tshirt519@gmail.com>
2021-10-28 17:11:00 +0900

    ✨ 🎨 product詳細画面実装

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from app import views
 
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
+  path('product/<slug>', views.ItemDetailView.as_view(), name='product'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View
 from .models import Item
 
-class IndexView(TemplateView):
+class IndexView(View):
   def get(self, request, *args, **kwargs):
     item_data = Item.objects.all()
     return render(request, 'app/index.html', {
+      'item_data': item_data
+    })
+
+class ItemDetailView(View):
+  def get(self, request, *args, **kwargs):
+    item_data = Item.objects.get(slug=self.kwargs['slug'])
+    return render(request, 'app/product.html', {
       'item_data': item_data
     })
