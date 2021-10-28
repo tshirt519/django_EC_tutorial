a5786879254937c6c89f0149d6e94bca3fcb7e73
tshirt519 <tshirt519@gmail.com>
2021-10-28 15:40:29 +0900

    custom authリポジトリからクローン

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Item(models.Model):
+  title = models.CharField(max_length=100)
+  price = models.IntegerField()
+  categroy = models.CharField(max_length=100)
+  slug = models.SlugField()
+  description = models.TextField()
+  image = models.ImageField(upload_to='images')
+  created = models.DateField('作成日', default=timezone.now)
+
+  def __str__(self):
+    return self.title
--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-class IndexView(LoginRequiredMixin, TemplateView):
-  template_name = 'app/index.html'
+from .models import Item
+
+class IndexView(TemplateView):
+  def get(self, request, *args, **kwargs):
+    item_data = Item.objects.all()
+    return render(request, 'app/index.html', {
+      'item_data': item_data
+    })
